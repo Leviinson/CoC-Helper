187af1569d164c1b708d8c5c54b970e27330ca47
Leviinson <votosi197@gmail.com>
2022-08-11 23:44:59 +0200

    added: separate vars for polling and throttling delays

--- bot_config.py
+++ bot_config.py
@@ -1,9 +1,15 @@
+'''Stores all necessary tokens; bot, dp objects.'''
+
 from aiogram import Dispatcher, Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 BOT_TOKEN = 'BOT_TOKEN'
 API_TOKEN = 'API_TOKEN'
+DelayPollClanWarLeagueMemberlist = 600
+DelayPollClanWarMemberlistAndRadeStatistic = 600
+ThrottlingDelay = 5
+
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot, storage = MemoryStorage())
