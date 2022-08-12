32ecf87d041258b0aa5c83e4575564448ded13b2
Leviinson <103764172+Leviinson@users.noreply.github.com>
2022-08-12 15:00:40 +0200

    Fix

--- bot/mysql_config.py
+++ bot/mysql_config.py
@@ -2,5 +2,5 @@ mysql_config_host = 'HOST'
 mysql_config_port = 'PORT' #int
 mysql_config_user = 'USER_NAME'
 mysql_config_password = "PASSWORD"
-mysql_config_db_name = 'DB_NAME'
+mysql_config_db_name = 'CoC_Helper'
 mysql_config_charset = 'utf8'
