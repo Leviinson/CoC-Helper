ab8f31d41d2149383a10a8cb419c9f01d896d1c9
Leviinson <votosi197@gmail.com>
2022-08-14 09:04:45 +0200

    added: table 'ChatUsers_nicknames'

--- bot/mysql_create_tables.sql
+++ bot/mysql_create_tables.sql
@@ -6,3 +6,4 @@ CREATE TABLE RadeMembers (member_name varchar(20), member_tag varchar(12), colle
 CREATE TABLE ChatAdmins (user_id BIGINT, user_name varchar(15), chat_id varchar(20), CONSTRAINT pk_chatMembers UNIQUE (user_id));
 CREATE TABLE ClanWarLeague_memberlist (member_name varchar(15), member_tag varchar(12), townHallLevel varchar(2), clan_tag varchar(10));
 CREATE TABLE ClanWarLeague_results (member_name varchar(15), member_tag varchar(12), stars TINYINT UNSIGNED DEFAULT 0, attacks TINYINT UNSIGNED DEFAULT 0, avg_score DECIMAL(4,3) DEFAULT 0, clan_tag varchar(12), CONSTRAINT pk_CWL_results UNIQUE (member_tag));
+CREATE TABLE ChatUsers_nicknames(user_id BIGINT, chat_id varchar(20), user_nickname varchar(30), CONSTRAINT pk_userid UNIQUE (user_id));
